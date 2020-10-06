69e22440256219abe9ca63527f96d3baf5897ffa
Denis <yourjest@gmail.com>
2020-10-06 20:13:54 +0300

    last snaphot

--- lab2.cc
+++ lab2.cc
@@ -38,49 +38,30 @@ void blending_simple(Mat img1, Mat img2, Mat dst, float alpha){
 	imwrite("simp.png", dst);
 }
 
-void blending_neon(const uint8_t* img1, const uint8_t* img2, uint8_t* dst, int num_pixels, float alpha){
+void blending_neon(const float32_t* img1, const float32_t* img2, float32_t* dst, int num_pixels, float alpha){
 	num_pixels /= 8;
 
-	uint16x8_t temp;
-	uint8x8x3_t result;
-  float16x8_t v_alpha = vdupq_n_f16(alpha);
+	float32x2x3_t result;
+  float32x2_t v_alpha = vdup_n_f32(alpha);
 
-  float16x8_t v_beta = vdupq_n_f16(1.0 - alpha);
+  float32x2_t v_beta = vdup_n_f32(1.0 - alpha);
 
-  float16x8_t img1_f16_b, img1_f16_g, img1_f16_r;
-  float16x8_t img2_f16_b, img2_f16_g, img2_f16_r;
-  float16x8_t res_b, res_g, res_r;
+  float32x2_t img1_f32_b, img1_f32_g, img1_f32_r;
+  float32x2_t img2_f32_b, img2_f32_g, img2_f32_r;
+  float32x2_t res_b, res_g, res_r;
 
-	for(int i =0; i<num_pixels; i++, img1+=8*3, img2+=8*3, dst+=8*3){
-		uint8x8x3_t img1_8 = vld3_u8(img1);
-		uint8x8x3_t img2_8 = vld3_u8(img2);
-
-    //converting pixel values to float
-    uint16x8_t img1_u16_b = vmovl_u8(img1_8.val[0]);
-    uint16x8_t img1_u16_g = vmovl_u8(img1_8.val[1]);
-    uint16x8_t img1_u16_r = vmovl_u8(img1_8.val[2]);
-
-    img1_f16_b = vreinterpretq_f16_u16(img1_u16_b);
-    img1_f16_g = vreinterpretq_f16_u16(img1_u16_g);
-    img1_f16_r = vreinterpretq_f16_u16(img1_u16_r);
-
-
-    uint16x8_t img2_u16_b = vmovl_u8(img2_8.val[0]);
-    uint16x8_t img2_u16_g = vmovl_u8(img2_8.val[1]);
-    uint16x8_t img2_u16_r = vmovl_u8(img2_8.val[2]);
-
-    img2_f16_b = vreinterpretq_f16_u16(img2_u16_b);
-    img2_f16_g = vreinterpretq_f16_u16(img2_u16_g);
-    img2_f16_r = vreinterpretq_f16_u16(img2_u16_r);
-
+	for(int i =0; i<num_pixels; i++, img1+=2*3, img2+=2*3, dst+=2*3){
+		float32x2x3_t img1_8 = vld3_f32(img1);
+		float32x2x3_t img2_8 = vld3_f32(img2);
+		
     //calculating blending weights
-    img1_f16_b = vdivq_f16(img1_f16_b, v_alpha);
-    img1_f16_g = vdivq_f16(img1_f16_g, v_alpha);
-    img1_f16_r = vdivq_f16(img1_f16_r, v_alpha);
+    img1_f32_b = vmul_n_f32(img1_8.val[0], alpha);
+    img1_f32_g = vmul_n_f32(img1_8.val[1], alpha);
+    img1_f32_r = vmul_n_f32(img1_8.val[2], alpha);
 
-    img2_f16_b = vdivq_f16(img2_f16_b, v_beta);
-    img2_f16_g = vdivq_f16(img2_f16_g, v_beta);
-    img2_f16_r = vdivq_f16(img2_f16_r, v_beta);
+    img2_f32_b = vmul_n_f32(img2_8.val[0], 1.0 - alpha);
+    img2_f32_g = vmul_n_f32(img2_8.val[1], 1.0 - alpha);
+    img2_f32_r = vmul_n_f32(img2_8.val[2], 1.0 - alpha);
 
     // img1_8.val[0] = vshr_n_u8(img1_8.val[0], 1);
 		// img1_8.val[1] = vshr_n_u8(img1_8.val[1], 1);
@@ -90,18 +71,11 @@ void blending_neon(const uint8_t* img1, const uint8_t* img2, uint8_t* dst, int n
     //     img2_8.val[1] = vshr_n_u8(img2_8.val[1], 1);
 		// img2_8.val[2] = vshr_n_u8(img2_8.val[2], 1);
 
-    //add, reinterpret values back to uint8, write to destination image
-    res_b = vaddq_f16(img1_f16_b, img2_f16_b);
-    res_g = vaddq_f16(img1_f16_g, img2_f16_g);
-    res_r = vaddq_f16(img1_f16_r, img2_f16_r);
-
-    //TODO vmov to u8
-
-    result.val[0] = vreinterpret_u8_f16(res_b);
-		result.val[1] = vreinterpret_u8_f16(res_g);
-		result.val[2] = vreinterpret_u8_f16(res_r);
-
-		vst3_u8(dst, result);
+    result.val[0] = vadd_f32(img1_f32_b, img2_f32_b);
+    result.val[1] = vadd_f32(img1_f32_g, img2_f32_g);
+    result.val[2] = vadd_f32(img1_f32_r, img2_f32_r);
+		
+		vst3_f32(dst, result);
 	}
 }
 
@@ -111,7 +85,6 @@ int main(int argc,char** argv)
 	uint8_t * img_arr2;
 	uint8_t * gray_arr_neon;
     double beta; double input;
-	double alpha = 0.5;
 	if (argc != 3) {
 		cout << "Usage: lab2 image_name1 image_name2" << endl;
 		return -1;
@@ -149,17 +122,14 @@ int main(int argc,char** argv)
 	int num_pixels = width*height;
 
 	Mat neon_dst(height, width, img1.type());
-  float16x8_t te1 = vdupq_n_f16(1.0);
-  float16x8_t te2 = vdupq_n_f16(0.2);
-  float16x8_t re = vaddq_f16(te1, te2);
 
-//  auto t1_neon = chrono::high_resolution_clock::now();
-	//blending_neon(img_arr1, img_arr2, neon_dst.data, num_pixels);
-//  auto t2_neon = chrono::high_resolution_clock::now();
- // auto duration_neon = chrono::duration_cast<chrono::microseconds>(t2_neon-t1_neon).count();
-  //cout << "Neon duraion" << endl;
-  //cout << duration_neon << endl;
-  //imwrite("neon.png", neon_dst);
+  auto t1_neon = chrono::high_resolution_clock::now();
+  blending_neon((float32_t*)img_arr1, (float32_t*)img_arr2, (float32_t*)neon_dst.data, num_pixels, 0.5);
+  auto t2_neon = chrono::high_resolution_clock::now();
+ auto duration_neon = chrono::duration_cast<chrono::microseconds>(t2_neon-t1_neon).count();
+ cout << "Neon duraion" << endl;
+ cout << duration_neon << endl;
+  imwrite("neon_1.png", neon_dst);
 
 	Mat simple_dst(height, width, img1.type());
   auto t1_simp = chrono::high_resolution_clock::now();
