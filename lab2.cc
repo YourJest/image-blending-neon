7c9aaedac6226fddca4ea15d3ea30ecb8c9d2f13
Denis Tropnikov <yourjest@gmail.com>
2020-10-12 11:07:22 +0000

    Added any alpha value support; removed makefile from .gitignore; fixed double saving of simple image

--- lab2.cc
+++ lab2.cc
@@ -16,147 +16,215 @@ void blending_by_func(Mat img1, Mat img2, float alpha, Mat dst){
 }
 
 void blending_simple(Mat img1, Mat img2, Mat dst, float alpha){
-	int cn1 = img1.channels();
-	int cn2 = img2.channels();
+  int cn1 = img1.channels();
+  int cn2 = img2.channels();
 
-	uint8_t* pixelPtr1 = (uint8_t*)img1.data;
-	uint8_t* pixelPtr2 = (uint8_t*)img2.data;
-	Scalar_<uint8_t> bgrPixel;
+  uint8_t* pixelPtr1 = (uint8_t*)img1.data;
+  uint8_t* pixelPtr2 = (uint8_t*)img2.data;
+  Scalar_<uint8_t> bgrPixel;
 
-	for(int i = 0; i < img1.rows; i++){
-		for(int j = 0; j < img1.cols; j++){
-			Vec3b & c_img1 = img1.at<Vec3b>(i,j);
-			Vec3b & c_img2 = img2.at<Vec3b>(i,j);
-			Vec3b & c_dst = dst.at<Vec3b>(i,j);
+  for(int i = 0; i < img1.rows; i++){
+    for(int j = 0; j < img1.cols; j++){
+      Vec3b & c_img1 = img1.at<Vec3b>(i,j);
+      Vec3b & c_img2 = img2.at<Vec3b>(i,j);
+      Vec3b & c_dst = dst.at<Vec3b>(i,j);
 
-			c_dst.val[0] = c_img1.val[0] * alpha + c_img2[0] * (1.0 - alpha);
-			c_dst.val[1] = c_img1.val[1] * alpha + c_img2[1] * (1.0 - alpha);
-			c_dst.val[2] = c_img1.val[2] * alpha + c_img2[2] * (1.0 - alpha);
-		}
-	}
-
-	imwrite("simp.png", dst);
+      c_dst.val[0] = c_img1.val[0] * alpha + c_img2[0] * (1.0 - alpha);
+      c_dst.val[1] = c_img1.val[1] * alpha + c_img2[1] * (1.0 - alpha);
+      c_dst.val[2] = c_img1.val[2] * alpha + c_img2[2] * (1.0 - alpha);
+    }
+  }
 }
 
-void blending_neon(const float32_t* img1, const float32_t* img2, float32_t* dst, int num_pixels, float alpha){
-	num_pixels /= 8;
+void blending_neon(const uint8_t* img1, const uint8_t* img2, uint8_t* dst, int num_pixels, float alpha){
+  num_pixels /= 8;
 
-	float32x2x3_t result;
-  float32x2_t v_alpha = vdup_n_f32(alpha);
+  uint8x8x3_t result;
+  bool first = true;
+  for(int i =0; i<num_pixels; i++, img1+=8*3, img2+=8*3, dst+=8*3){
+    uint8x8x3_t img1_u8 = vld3_u8(img1);
+    uint8x8x3_t img2_u8 = vld3_u8(img2);
+    /*Debug print of loaded value
+    if(first){
+      cout << "orig_image first values: "<< endl;
+      uint8_t r_d[8];
+      for(int k = 0; k<3;k++){
+        vst1_u8(r_d, img2_u8.val[k]);
+        for(int l= 0; l<8;l++){
+          cout << to_string(r_d[l]) << ' ';
+        }
+        cout << endl;
+      }
+      cout << endl;
+    }*/
+    for(int j = 0; j < 3; j++){
+      uint16x8_t img1_u16 = vmovl_u8(img1_u8.val[j]);
+      uint16x8_t img2_u16 = vmovl_u8(img2_u8.val[j]);
 
-  float32x2_t v_beta = vdup_n_f32(1.0 - alpha);
+      uint32x4_t img1_u32l, img1_u32h, img2_u32l, img2_u32h;
+      float32x4_t img1_f32l, img1_f32h, img2_f32l, img2_f32h;
+      img1_u32l = vmovl_u16(vget_low_u16(img1_u16));
+      img1_u32h = vmovl_u16(vget_high_u16(img1_u16));
+      img1_f32l = vcvtq_f32_u32(img1_u32l);
+      img1_f32h = vcvtq_f32_u32(img1_u32h);
 
-  float32x2_t img1_f32_b, img1_f32_g, img1_f32_r;
-  float32x2_t img2_f32_b, img2_f32_g, img2_f32_r;
-  float32x2_t res_b, res_g, res_r;
+      img2_u32l = vmovl_u16(vget_low_u16(img2_u16));
+      img2_u32h = vmovl_u16(vget_high_u16(img2_u16));
+      img2_f32l = vcvtq_f32_u32(img2_u32l);
+      img2_f32h = vcvtq_f32_u32(img2_u32h);
 
-	for(int i =0; i<num_pixels; i++, img1+=2*3, img2+=2*3, dst+=2*3){
-		float32x2x3_t img1_8 = vld3_f32(img1);
-		float32x2x3_t img2_8 = vld3_f32(img2);
-		
-    //calculating blending weights
-    img1_f32_b = vmul_n_f32(img1_8.val[0], alpha);
-    img1_f32_g = vmul_n_f32(img1_8.val[1], alpha);
-    img1_f32_r = vmul_n_f32(img1_8.val[2], alpha);
+      img1_f32h = vmulq_n_f32(img1_f32h, alpha);
+      img1_f32l = vmulq_n_f32(img1_f32l, alpha);
+      img2_f32h = vmulq_n_f32(img2_f32h, 1.0 - alpha);
+      img2_f32l = vmulq_n_f32(img2_f32l, 1.0 - alpha);
 
-    img2_f32_b = vmul_n_f32(img2_8.val[0], 1.0 - alpha);
-    img2_f32_g = vmul_n_f32(img2_8.val[1], 1.0 - alpha);
-    img2_f32_r = vmul_n_f32(img2_8.val[2], 1.0 - alpha);
+      float32x4_t res_f32h = vaddq_f32(img1_f32h, img2_f32h);
+      float32x4_t res_f32l = vaddq_f32(img1_f32l, img2_f32l);
+      /* Convertion debugging
+      if(first){
+        cout << "float vector convert: " << endl;
+        float32_t r_dl[4];
+        float32_t r_dh[4];
+        vst1q_f32(r_dl, res_f32l);
+        vst1q_f32(r_dh, res_f32h);
+        for(int k = 0; k<4;k++){
+          cout << to_string(r_dl[k]) << ' ';
+        }
+        for(int k = 0; k<4;k++){
+          cout << to_string(r_dh[k]) << ' ';
+        }
+        cout << endl;
+      }*/
 
-    // img1_8.val[0] = vshr_n_u8(img1_8.val[0], 1);
-		// img1_8.val[1] = vshr_n_u8(img1_8.val[1], 1);
-		// img1_8.val[2] = vshr_n_u8(img1_8.val[2], 1);
-    //
-		// img2_8.val[0] = vshr_n_u8(img2_8.val[0], 1);
-    //     img2_8.val[1] = vshr_n_u8(img2_8.val[1], 1);
-		// img2_8.val[2] = vshr_n_u8(img2_8.val[2], 1);
+      uint32x4_t res_u32h = vcvtq_u32_f32(res_f32h);
+      uint32x4_t res_u32l = vcvtq_u32_f32(res_f32l);
 
-    result.val[0] = vadd_f32(img1_f32_b, img2_f32_b);
-    result.val[1] = vadd_f32(img1_f32_g, img2_f32_g);
-    result.val[2] = vadd_f32(img1_f32_r, img2_f32_r);
-		
-		vst3_f32(dst, result);
-	}
+      uint16x8_t res = vmovn_high_u32(vget_low_u16(res), res_u32l);
+      res = vmovn_high_u32(vget_high_u16(res), res_u32h);
+      result.val[j] = vmovn_u16(res);
+    }
+    /*Result debugging
+    if(first){
+      first = false;
+      uint8_t r_d[8];
+      std::cout << "result verctor after blending filter" << '\n';
+      for(int k = 0; k<3;k++){
+        vst1_u8(r_d, result.val[k]);
+        for(int l= 0; l<8;l++){
+          cout << to_string(r_d[l]) << ' ';
+        }
+        cout << endl;
+      }
+      cout << endl;
+    }
+    */
+    vst3_u8(dst, result);
+  }
 }
 
 int main(int argc,char** argv)
 {
-	uint8_t * img_arr1;
-	uint8_t * img_arr2;
-	uint8_t * gray_arr_neon;
-    double beta; double input;
-	if (argc != 3) {
-		cout << "Usage: lab2 image_name1 image_name2" << endl;
-		return -1;
-	}
+  if (argc != 4) {
+    cout << "Usage: lab2 image_name1 image_name2 alpha" << endl;
+    return -1;
+  }
+  uint8_t * img1_arr;
+  uint8_t * img2_arr;
 
-	Mat img1;
-	img1 = imread(argv[1], cv::IMREAD_COLOR);
-	if (!img1.data) {
-	  	cout << "Could not open the image" << endl;
-	   	return -1;
-	}
-	if (img1.isContinuous()) {
-	    img_arr1 = img1.data;
-	}
-	else {
-	    cout << "data is not continuous" << endl;
-	    return -2;
-	}
-	Mat img2;
-	img2 = imread(argv[2], cv::IMREAD_COLOR);
-	if (!img2.data) {
-	  	cout << "Could not open the image" << endl;
-	   	return -1;
-	}
-	if (img2.isContinuous()) {
-	    img_arr2 = img2.data;
-	}
-	else {
-	    cout << "data is not continuous" << endl;
-	    return -2;
-	}
+  Mat img1;
+  img1 = imread(argv[1], cv::IMREAD_COLOR);
+  if (!img1.data) {
+      cout << "Could not open the image" << endl;
+       return -1;
+  }
+  if (img1.isContinuous()) {
+      img1_arr = img1.data;
+  }
+  else {
+      cout << "data is not continuous" << endl;
+      return -2;
+  }
+  Mat img2;
+  img2 = imread(argv[2], cv::IMREAD_COLOR);
+  if (!img2.data) {
+      cout << "Could not open the image" << endl;
+       return -1;
+  }
+  if (img2.isContinuous()) {
+      img2_arr = img2.data;
+  }
+  else {
+      cout << "data is not continuous" << endl;
+      return -2;
+  }
+  float alpha = stof(argv[3]);
+  int width = img1.cols;
+  int height = img1.rows;
+  int num_pixels = width*height;
 
-	int width = img1.cols;
-	int height = img1.rows;
-	int num_pixels = width*height;
-
-	Mat neon_dst(height, width, img1.type());
-
+  Mat neon_dst(height, width, img1.type());
   auto t1_neon = chrono::high_resolution_clock::now();
-  blending_neon((float32_t*)img_arr1, (float32_t*)img_arr2, (float32_t*)neon_dst.data, num_pixels, 0.5);
+  blending_neon(img1_arr, img2_arr, neon_dst.data, num_pixels, alpha);
   auto t2_neon = chrono::high_resolution_clock::now();
- auto duration_neon = chrono::duration_cast<chrono::microseconds>(t2_neon-t1_neon).count();
- cout << "Neon duraion" << endl;
- cout << duration_neon << endl;
+  auto duration_neon = chrono::duration_cast<chrono::microseconds>(t2_neon-t1_neon).count();
+  cout << "Neon duraion" << endl;
+  cout << duration_neon << endl;
   imwrite("neon_1.png", neon_dst);
 
-	Mat simple_dst(height, width, img1.type());
+  /* Testing Uint8 to float convertion
+  uint8x8_t test_u8 = vld1_u8(img1_arr);
+  uint8_t data_u8[8];
+  vst1_u8(data_u8, test_u8);
+  for(int i = 0; i < 8; i++){
+    cout << to_string(data_u8[i]) << " ";
+  }
+  cout << endl;
+  uint8x8_t test2_u8 = vld1_u8(img2_arr);
+  uint8_t data2_u8[8];
+  vst1_u8(data2_u8, test2_u8);
+  for(int i = 0; i < 8; i++){
+    cout << to_string(data2_u8[i]) << " ";
+  }
+  cout << endl;
+  uint16x8_t test_u16 = vmovl_u8(test_u8);
+  uint32x4_t i32vl, i32vh;
+  float32x4_t f32vl, f32vh;
+  i32vl = vmovl_u16(vget_low_u16(test_u16));
+  i32vh = vmovl_u16(vget_high_u16(test_u16));
+  f32vl = vcvtq_f32_u32(i32vl);
+  f32vh = vcvtq_f32_u32(i32vh);
+  f32vl = vmulq_n_f32(f32vl, 0.5);
+  f32vh = vmulq_n_f32(f32vh, 0.5);
+
+  i32vl = vcvtq_u32_f32(f32vl);
+  i32vh = vcvtq_u32_f32(f32vh);
+
+  uint16x8_t u16v = vmovn_high_u32(vget_low_u16(u16v), i32vl);
+  u16v = vmovn_high_u32(vget_high_u16(u16v), i32vh);
+  uint8x8_t u8v = vmovn_u16(u16v);
+  uint8_t data[8];
+  vst1_u8(data, u8v);
+  for(int i = 0; i < 8; i++){
+    cout << to_string(data[i]) << " ";
+  }
+  cout << endl; */
+  Mat simple_dst(height, width, img1.type());
   auto t1_simp = chrono::high_resolution_clock::now();
-  blending_simple(img1, img2, simple_dst, 0.3);
+  blending_simple(img1, img2, simple_dst, alpha);
   auto t2_simp = chrono::high_resolution_clock::now();
   auto duration_simp = chrono::duration_cast<chrono::microseconds>(t2_simp-t1_simp).count();
   cout << "Simple duraion" << endl;
   cout << duration_simp << endl;
-	imwrite("simp.png", simple_dst);
+  imwrite("simp.png", simple_dst);
 
-	Mat func_dst(height, width, img1.type());
+  Mat func_dst(height, width, img1.type());
   auto t1_func = chrono::high_resolution_clock::now();
-	blending_by_func(img1, img2, 0.3, func_dst);
+  blending_by_func(img1, img2, alpha, func_dst);
   auto t2_func = chrono::high_resolution_clock::now();
   auto duration_func = chrono::duration_cast<chrono::microseconds>(t2_func-t1_func).count();
   cout << "addWeighted function duraion" << endl;
   cout << duration_func << endl;
   imwrite("func.png", func_dst);
-	/*
-	auto t1_neon = chrono::high_resolution_clock::now();
-	rgb_to_gray_neon(rgb_arr, gray_arr_neon, num_pixels);
-	auto t2_neon = chrono::high_resolution_clock::now();
-	auto duration_neon = chrono::duration_cast<chrono::microseconds>(t2_neon-t1_neon).count();
-	cout << "rgb_to_gray_neon" << endl;
-	cout << duration_neon << " us" << endl;
 
-	imwrite("gray_neon.png", gray_image_neon);
-  */
-    return 0;
+  return 0;
 }
